aaa2ed3508758e9aa0868431f4475aeef05716af
AJ Speller <ajspeller@gmail.com>
2020-07-13 19:32:03 -0400

    Add application setup

--- app.js
+++ app.js
@@ -4,9 +4,27 @@ require('./database/db');
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const path = require('path');
+const swig = require('swig');
+const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
 
+const routes = require('./routes/index');
+
+// logging
 app.use(morgan('dev'));
+
+// view engine
+app.engine('html', swig.renderFile);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'html');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/', routes);
 
 app.listen(PORT, () => {
   console.log('Server listening on port: ', PORT);
